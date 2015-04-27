90cfc21cf3424599adf8ef5cb0a58d1f0e7a4d19
cnso <hdmj@sina.com>
2015-04-27 17:23:32 +0800

    123445

--- app/src/main/java/com/kyle/activity/Activity_cityLook.java
+++ app/src/main/java/com/kyle/activity/Activity_cityLook.java
@@ -8,6 +8,7 @@ import android.view.MenuItem;
 
 public class Activity_cityLook extends ActionBarActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
